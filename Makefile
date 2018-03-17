a1414cdc3d0302c42c9ccf704eeb1bcd346b9b29
Faisal Khan <faisal.nust@gmail.com>
2018-03-17 17:42:52 -0500

    Added eigen-git-mirror as submodule

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC = g++
 
 CXXFLAGS = -std=c++11 -Wall -g -fPIC 
 
-INCLUDES = -I./
+INCLUDES = -I./eigen-git-mirror/
 
 LIBDIR = 
 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
+//
 
-#include "headers.h"
-#include "matrix_basics.h"
-#include "matrix_matrix_operations.h"
-#include "indexing_operations.h"
+// Header file for dense example. 
+#include <iostream>
 
-int main()
-{
-    // Matrix_Basic_Operations();
-    // Matrix_Matrix_Operations();
-    Indexing_Operations();
+#include <Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
+
+void QuickExample() {
+	Eigen::MatrixXi A(2, 3);
+  A <<  1, 1, 1,
+        0, 1, 0;
+  cout << "Matrix A\n" << A <<endl;
+  Eigen::MatrixXi B(3, 2);
+  B << 2, 0,
+       3, 0,
+       1, 2;
+	cout << "Matrix B \n" << B <<endl;
+
+	Eigen::MatrixXi C = A + B.transpose();
+  cout<<"A + B.transpose() \n" << C<<endl;
+}
+
+
+
+int main() {
+	cout<<"\nA simple matrix example : A + B.transpose()\n";
+	cout<<"_______________________________________________\n";
+
+  QuickExample();
+
+  cout<<"\nA typedef example\n";
+  cout<<"_______________________________________________\n";
+  MatrixTypeDefs();
 }
